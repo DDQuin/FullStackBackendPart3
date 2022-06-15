732aad71fce4f9c4adae7206b6ba8127943e3861
DDQuin <ddquin420@gmail.com>
2022-06-15 20:37:39 +0100

    ex 3.8

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(function (tokens, req, res) {
+    let jsonBody = "";
+    const type = tokens.method(req, res)
+    if (type === "POST") {
+        jsonBody = JSON.stringify(req.body)
+    }
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      `${jsonBody}`
+    ].join(' ')
+  }))
 
 let persons = [
     { 
